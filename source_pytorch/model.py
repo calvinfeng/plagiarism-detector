d4152e2b5a9b8040277d8e2d3d9d6783a3bf7a6a
Calvin Feng <calvin.j.feng@gmail.com>
2020-04-23 20:59:18 -0700

    Commit all project materials

---
+++ source_pytorch/model.py
@@ -0,0 +1,44 @@
+# torch imports
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+## TODO: Complete this classifier
+class BinaryClassifier(nn.Module):
+    """
+    Define a neural network that performs binary classification.
+    The network should accept your number of features as input, and produce 
+    a single sigmoid value, that can be rounded to a label: 0 or 1, as output.
+    
+    Notes on training:
+    To train a binary classifier in PyTorch, use BCELoss.
+    BCELoss is binary cross entropy loss, documentation: https://pytorch.org/docs/stable/nn.html#torch.nn.BCELoss
+    """
+
+    ## TODO: Define the init function, the input params are required (for loading code in train.py to work)
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        """
+        Initialize the model by setting up linear layers.
+        Use the input parameters to help define the layers of your model.
+        :param input_features: the number of input features in your training/test data
+        :param hidden_dim: helps define the number of nodes in the hidden layer(s)
+        :param output_dim: the number of outputs you want to produce
+        """
+        super(BinaryClassifier, self).__init__()
+        self.fully_connected_1 = nn.Linear(input_dim, hidden_dim)
+        self.fully_connected_2 = nn.Linear(hidden_dim, output_dim)
+        self.drop_out = nn.Dropout(0.3)
+        self.sigmoid = nn.Sigmoid()
+
+    ## TODO: Define the feedforward behavior of the network
+    def forward(self, x):
+        """
+        Perform a forward pass of our model on input features, x.
+        :param x: A batch of input features of size (batch_size, input_features)
+        :return: A single, sigmoid-activated value as output
+        """
+        x = F.relu(self.fully_connected_1(x)) # First activation
+        x = self.drop_out(x)
+        x = self.sigmoid(self.fully_connected_2(x)) # Second activation
+        return x
+    
